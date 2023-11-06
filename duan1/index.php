910f660b1fd8bfdb45d15f448276ef978a72199a
Tunnbeoo <duongminhtrung.12c4htk@gmail.com>
2023-11-07 01:54:29 +0700

    Update

---
+++ duan1/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+        
+        body{
+            margin: auto;
+        }
+        header{
+            width: auto;
+            height: 100px;
+            background-color: red;
+            display: flex;
+        }
+        header>.logo_text{
+            margin: 9px 40px 0 40px;
+            background-color: aqua;
+            height: 80px;
+            width: 80px;
+        }
+        header>.seacher{
+            width: 600px;
+            height: 40px;
+            background-color: aqua;
+            margin-left: 200px;
+            margin-top: 30px;
+        }
+        header>.seacher_logo{
+            float: right;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <div class="logo_text"></div>
+        <div class="seacher">
+            <div class="seacher_logo"></div>
+        </div>
+    </header>
+    <nav>
+
+    </nav>
+</body>
+</html>
