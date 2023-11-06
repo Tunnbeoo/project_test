a5288a70ed0a17db1d81d07c83cf9b4cf15ced2e
Tunnbeoo <duongminhtrung.12c4htk@gmail.com>
2023-11-06 17:09:50 +0700

    Update

---
+++ admin/themsp.php
@@ -0,0 +1,30 @@
+<?php
+if ( isset($_GET['id'])==true){
+$id = (int) $_GET['id'];}
+else $id=0;
+$loai = lay1sp($id);
+?>
+<form action="suasanpham_.php" method="post" id="frm1">
+<h2>SUA SẢN PHẨM</h2>
+<div> 
+    <label>Tên sản phẩm </label> <input type="text" class="txt" value="<?php echo $loai[1]?>" name="ten_loai">
+</div>
+<div> 
+    <label>Thứ tự </label> <input type="number" class="txt" min="1" name="thutu">
+</div>
+<div> 
+    <label>Ẩn hiện </label>
+    <input type="radio" value="0" name="anhien"> Ẩn
+    <input type="radio" value="1" name="anhien" checked> Hiện
+</div>
+<div> <label></label> <button type="submit">Lưu thông tin san pham</button> </div>
+</form>
+<style>
+#frm1 { width: 800px; margin: auto; border: 1px solid black;}
+#frm1 h2 { background: black; color:white; padding: 10px; }
+#frm1 div { padding: 10px 10px; border-bottom: 1px dotted black; }
+#frm1 label {display: inline-block;width:120px }
+#frm1 .txt { padding: 5px; width: calc(100% - 130px); outline: none;}
+#frm1 button{ width: 120px; height: 30px;}
+
+</style>
