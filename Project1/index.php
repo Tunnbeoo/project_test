4f7968604bd508e186a2046a98948538b059ede2
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-11-07 23:13:31 +0700

    Update index.php

--- Project1/index.php
+++ Project1/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/git_project_1/Project 1/index.css">
+    <link rel="stylesheet" href="/Project1/index.css">
 </head>
 <body>
 <header>
@@ -457,5 +457,6 @@
         </div>
     </div>
 </header>
+<div>haha</div>
 </body>
 </html>
