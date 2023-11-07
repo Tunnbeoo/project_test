00207a7632f8578e066d738345293f09c28fbcd2
Tunnbeoo <duongminhtrung.12c4htk@gmail.com>
2023-11-07 23:13:34 +0700

    header

--- Project1/index.php
+++ Project1/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/git_project_1/Project 1/index.css">
+    <link rel="stylesheet" href="/git_project_1/Project1/index.css">
 </head>
 <body>
 <header>
