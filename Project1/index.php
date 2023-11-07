997ba10cf954bb75366ae1d4a10f91ca643c9de2
Tunnbeoo <duongminhtrung.12c4htk@gmail.com>
2023-11-07 23:26:02 +0700

    logo

--- Project1/index.php
+++ Project1/index.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="align-items-center justify-content-between d-flex">
             <a class="logo" href="/">
-                <img alt="laptopaz.com chuyên laptop cũ, bán laptop cũ uy tín Hà Nội và toàn quốc" src="/media/banner/logo_laptopaz (2).jpg" class="img-fluid">
+                <img alt="laptopaz.com chuyên laptop cũ, bán laptop cũ uy tín Hà Nội và toàn quốc" src="/git_project_1/Project1/img/COMPUTER.png" class="img-fluid">
             </a>
 
             <div class="search-form">
