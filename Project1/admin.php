228b662afda3a9ef9a8f6df4249cb69193d6febc
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-11-24 00:19:32 +0700

    clmm

---
+++ Project1/admin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Trang Admin</title>
+    <link rel="stylesheet" href="/Project1/admin.css">
+</head>
+<body>
+
+    <header>
+        <a href=""><img src="/Project1/img/logout.png" alt=""></a>
+    </header>
+
+    <nav>
+        <ul>
+            <img src="/Project1/img/logokh.png" alt="">
+            <li><a href="#">Bảng điều khiển</a></li>
+            <li><a href="#">Quản lý Sản phẩm</a></li>
+            <li><a href="#">Quản lý Người dùng</a></li>
+            <li><a href="#">Thống kê</a></li>
+            <li><a href="#">Cài đặt</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section class="dashboard">
+            <h2>Bảng điều khiển</h2>
+            <!-- Nội dung bảng điều khiển -->
+        </section>
+    </main>
+
+
+</body>
+</html>
