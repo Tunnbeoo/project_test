38cb56b052c1ad7acee4c40778e6c9ac81d97d9d
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-11-26 11:29:29 +0700

    1

--- Project1/home.php
+++ Project1/home.php
@@ -23,8 +23,8 @@
 
         <!-- Thêm các div với hình ảnh khác tương tự -->
 
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        <!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a> -->
     </div>
     
 
