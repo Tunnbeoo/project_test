69a91718fd444ccc0f05daddba997aa6088ddc62
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-11-26 22:16:57 +0700

    11

---
+++ Project1/tin-khuyen-mai.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="tinkhuyenmai.css">
+</head>
+<body>
+    <div>
+        <img src="/Project1/img/quangcao2.png" alt="">
+    </div>
+    <div class="tintuc-right">
+        <h3>TIN NỔI BẬT</h3>
+        
+        <p> Dell Inspiron 14 7435 2in1 2023 - ĐẸP, ĐỘC LẠ, ĐA NĂNG mà lại còn RẺ</p>
+        <p> MỪNG TỰU TRƯỜNG - GIÁ YÊU THƯƠNG, GIẢM TỚI 5 TRIỆU ĐỒNG</p>
+        <p> Microsoft bất ngờ tung AI Copilot cho Windows 10?</p>
+        <p> Acer Predator Helios Neo 2023 - chiến thần hiệu năng, bá thủ tản nhiệt!</p>
+        
+        <h3>TIN KHUYẾN MÃI</h3>
+        <img src="/Project1/img/quangcao2.png" alt="">
+        <h5>MỪNG TỰU TRƯỜNG - GIÁ YÊU THƯƠNG, GIẢM TỚI 5 TRIỆU ĐỒNG</h5>
+        <div class="alltinsanpham">
+        <div class="tinsanpham">
+            <img src="/Project1/img/quangcao1.png" alt="">
+            <p>TOP 4 Laptop Gaming Dell Đáng Mua Nhất Tầm Giá 23 - 29 Triệu Thời Điểm Hiện Tại!!!</p>
+        </div>
+        <div class="tinsanpham">
+            <img src="/Project1/img/quangcao3.png" alt="">
+            <p>TOP 4 Laptop Gaming Sở Hữu Card Đồ Họa RTX 4060 Không Thể Bỏ Lỡ</p>
+        </div>
+        <div class="tinsanpham">
+            <img src="/Project1/img/quangcao4.png" alt="">
+            <p>TOP 4 Ultrabook 2-in-1 HOT Nhất Mùa Tựu Trường 2023</p>
+        </div>
+        <div class="tinsanpham">
+            <img src="/Project1/img/quangcao5.png" alt="">
+            <p>TOP 5 Laptop Gaming 20-25 Triệu Bán Chạy Nhất 2023</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
