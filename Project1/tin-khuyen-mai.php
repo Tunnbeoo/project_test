df6860743edd7a9170ce56543819c2bd5f92cc08
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-11-26 23:04:53 +0700

    tin tuc ne

--- Project1/tin-khuyen-mai.php
+++ Project1/tin-khuyen-mai.php
@@ -8,7 +8,45 @@
 </head>
 <body>
     <div>
+        <div>
         <img src="/Project1/img/quangcao2.png" alt="">
+        </div>
+        <div class="tintucchitiet">
+            <img src="/Project1/img/tintuc1.png" alt="">
+           <div class="texttintuc"><h6>TOP 4 Laptop Gaming Dell Đáng Mua Nhất Tầm Giá 23 - 29 Triệu Thời Điểm Hiện Tại!!!</h6>
+            <p>Ngoài việc hỗ trợ cho việc học tập và làm việc, 
+            Laptop cũng là một công cụ hữu ích giúp bạn thư giãn bằng cách chơi Game. 
+            Dell được biết đến như một trong những thương hiệu laptop chơi game phổ biến và được ưa chuộng nhất từ ...</p>
+            <p class="date">15-09-2023, 5:07 pm</p>
+            </div> 
+        </div>
+        <div class="tintucchitiet">
+            <img src="/Project1/img/tintuc2.jpg" alt="">
+            <div class="texttintuc"><h6>TOP 4 Ultrabook 2-in-1 HOT Nhất Mùa Tựu Trường 2023</h6>
+            <p>Mùa tựu trường 2023 - 2024 đã đến. Chắc hẳn có rất nhiều bạn học sinh, 
+                sinh viên đang quan tâm đến những chiếc Laptop có thể hỗ trợ tốt nhất cho việc học tập, giải trí của mình. 
+                Tuy nhiên trên thị trường hiện nay, có rất nhiều ...</p>
+            <p class="date">15-09-2023, 5:07 pm</p>
+            </div> 
+        </div>
+        <div class="tintucchitiet">
+            <img src="/Project1/img/tintuc3.jpg" alt="">
+            <div class="texttintuc"><h6>TOP 4 Laptop Gaming Sở Hữu Card Đồ Họa RTX 4060 Không Thể Bỏ Lỡ</h6>
+            <p>Laptop gaming là dòng laptop được sản xuất để phục vụ người chơi game, 
+                vì vậy nó đòi hỏi cấu hình cao cũng như đáp ứng được chất lượng đồ họa để đảm bảo quá trình chơi được liền mạch, 
+                không giật lag. Ngoài ra, nhiều đối ...</p>
+            <p class="date">11-09-2023, 3:24 pm</p>
+            </div> 
+        </div>
+        <div class="tintucchitiet">
+            <img src="/Project1/img/tintuc4.png" alt="">
+            <div class="texttintuc"><h6>TOP 3 Ultrabook Cảm Ứng HOT Nhất Mùa Tựu Trường 2023</h6>
+            <p>Vậy là mùa tựu trường 2023 - 2024 lại đang tới. 
+                Các bạn học sinh, sinh viên đã bắt đầu chuẩn bị cho mình hành trang để hỗ trợ việc học tập, giải trí. 
+                Và một chiếc laptop chắc chắn sẽ là công cụ đắc lực đối với học tập và ...</p>
+            <p class="date">04-08-2023, 4:18 pm</p>
+            </div> 
+        </div>
     </div>
     <div class="tintuc-right">
         <h3>TIN NỔI BẬT</h3>
@@ -31,11 +69,11 @@
             <p>TOP 4 Laptop Gaming Sở Hữu Card Đồ Họa RTX 4060 Không Thể Bỏ Lỡ</p>
         </div>
         <div class="tinsanpham">
-            <img src="/Project1/img/quangcao4.png" alt="">
+            <img src="/Project1/img/quangcao4.webp" alt="">
             <p>TOP 4 Ultrabook 2-in-1 HOT Nhất Mùa Tựu Trường 2023</p>
         </div>
         <div class="tinsanpham">
-            <img src="/Project1/img/quangcao5.png" alt="">
+            <img src="/Project1/img/quangcao5.jpg" alt="">
             <p>TOP 5 Laptop Gaming 20-25 Triệu Bán Chạy Nhất 2023</p>
         </div>
         </div>
