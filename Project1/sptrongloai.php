5a4b85f1e1f125add60a02146fd71abd80f94166
bin0120120 <150657888+bin0120120@users.noreply.github.com>
2023-11-26 21:18:26 +0700

    bú

--- Project1/sptrongloai.php
+++ Project1/sptrongloai.php
@@ -13,28 +13,35 @@ $page_next = $page_num +1;
 $listsp = laysp_trongloai($id, $page_num, $page_size);
 $tenloai = laytenloai($id);
 ?>
-<div id="sp_trongloai" >
-<h3> <?php echo $tenloai ?></h3>
-<div id="data" > 
-<?php foreach($listsp as $sp) { ?>    
-<div class="sp">
-<h3> <a href="#"> <?php echo $sp[1]; ?> </a> </h3>
-<a href="#"> <img src="<?php echo $sp[3]; ?> " > </a>
-<p>Giá: <b><?php echo $sp[2]; ?></b> VNĐ </p>
-<!-- <p> <button onclick="chonsp('<?=$sp[0]?>','<?=$sp[1]?>','<?=$sp[3]?>','<?=$sp[2]?>')">
- Chọn</button></p> -->
-</div>
-    <?php } ?>
-</div>
-<div class="phantrang">
-   <?php if($page_prev>1) {?>
-      <a href="index.php?page=loai&id =<?=$id?>&pn=<?$page_prev?>">trang truoc</a>
-   <?php } ?>
-   <?php if($page_next<100) {?>
-      <a href="index.php?page=loai&id =<?=$id?>&pn=<?$page_prev?>">trang sau</a>
-   <?php } ?>
-</div>
-</div>
+
+
+
+<div class="container-product">
+  <div class="pro-head d-flex">
+    <div class="title">
+      <h5>
+        <a href="#"><?php echo $tenloai ?></a>
+      </h5>
+    </div>
+  </div>
+
+  <section class="product-container">
+    <?php foreach ($listsp as $sp) { ?>
+      
+        <div class="product">
+          <a href="index.php?page=sp&id=<?php echo $sp[0] ?>"> <img src="<?php echo $sp[3] ?>"> </a>
+          <h2><a href="index.php?page=sp&id=<?php echo $sp[0] ?>"><?php echo $sp[1] ?></a></h2>
+          <p>Mô tả ngắn về sản phẩm. Mô tả ngắn về sản phẩm. Mô tả ngắn về sản phẩm.</p>
+          <p class="price"><?php echo $sp[2] ?>VND</p>
+          
+
+
+                      <!-- Thêm các sản phẩm khác tương tự -->
+
+      
+  </div>
+<?php } ?>
+</section>
 <script>
    function chonsp(id,ten,hinh,gia){
       let cart = JSON.parse(localStorage.getItem('cart'));
@@ -48,16 +55,4 @@ $tenloai = laytenloai($id);
       localStorage.setItem("cart", JSON.stringify(cart));
    }
 </script>
-<style>
-   #sp_trongloai > #data {width: 200px;}
-#sp_trongloai > #data >.sp {
- border: 1px solid darkblue; padding: 10px; text-align: center;
-}
-#sp_trongloai > #data >.sp h3 { height: 60px; line-height: 100%; font-size: 20px;}
-#sp_trongloai > #data >.sp h3 a {text-decoration: none; color: black; }
-#sp_trongloai > #data >.sp img {
- width: 96%; height: 180px; border: 1px solid black;
-}
-#sp_trongloai > #data >.sp > p:nth-child(3) {font-size:20px;margin:15px;color:rgb(125, 125, 125);}
-#sp_trongloai > #data >.sp button { width: 90px; height: 30px; }
-</style>
+
