64b12cab52b1b41e1cc40d9258e8ecb4bd3a3c50
huydao12 <118437715+huydao12@users.noreply.github.com>
2023-12-06 20:57:46 +0700

    xoa file du admin

--- Project1/admin/admin/themsp_.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$ten_loai = trim(strip_tags($_POST['ten_sp']));
-$id_loai = (int) $_POST['id_loai'];
-$anhien = (int) $_POST['anhien'];
-require_once "../functions.php";
-$kq = chensp($ten_sp, $id_loai, $anhien);
-if($kq == true) header('location: admin.php?page=thongvao');
-?>
