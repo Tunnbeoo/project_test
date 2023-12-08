e7a02e48ad749dda2cb26158863b024e55db3052
Tunnbeoo <duongminhtrung.12c4htk@gmail.com>
2023-12-08 15:44:56 +0700

    thong tin

--- Project1/admin/khachhang.php
+++ Project1/admin/khachhang.php
@@ -1,20 +1,31 @@
-<?php  $list = listkhachhang(); ?>
+<?php $list = listkhachhang(); ?>
 <div id="danhsach">
-    <h1>Danh sách khach hang</h1>
+    <h1>Danh Sách Khách Hàng</h1>
     <div class="dshead">
-       <div>Id Khach hang</div><div>Tên khach hang</div><div>email</div><div>so dien thoai</div><div></div>
+        <div>Id Khách Hàng</div>
+        <div>Tên Khách Hàng</div>
+        <div>email</div>
+        <div>Số Điện Thoại</div>
+        <div></div>
     </div>
-    <?php foreach($list as $row){?>
-    <div class="dsdata">
-       <div><?php echo $row['id_user'];?></div>
-       <div><?php echo $row['ho'] . $row['ten'];?></div>
-       <div><?php echo $row['email'];?></div>
-       <div><?php echo $row['dienthoai'];?></div>
-       <div><a href="xoakh.php?id=<?=$row[0]?>">xoa</a>
-       </a></div>
-    </div><?php } ?>
+    <?php foreach ($list as $row) { ?>
+        <div class="dsdata">
+            <div><?php echo $row['id_user']; ?></div>
+            <div><?php echo $row['ho'] . $row['ten']; ?></div>
+            <div><?php echo $row['email']; ?></div>
+            <div><?php echo $row['dienthoai']; ?></div>
+            <div><a href="xoakh.php?id=<?= $row[0] ?>">Xóa</a>
+                </a></div>
+        </div><?php } ?>
 </div>
 <style>
+<<<<<<< HEAD
+    #danhsach {
+        width: 1000px;
+        margin: auto;
+        ;
+    }
+=======
     #danhsach { width: 1000px; margin: auto;;}
 #danhsach > h1 {background: darkblue; padding: 10px; color:white;font-size: 24px;}
 #danhsach > div {display: grid; grid-template-columns: 90px 400px 300px 150px 50px;}
@@ -22,5 +33,39 @@
 #danhsach > div > div {border: 1px solid darkblue; padding: 10px; text-align: center;}
 #danhsach a {text-decoration: none; color: wheat;font-weight: bold;}
 #danhsach a:hover { text-decoration: underline; color: black;}
+>>>>>>> 92dcbc86862439f2d9b05d01c16ffa00f621f59f
 
+    #danhsach>h1 {
+        background: #eef5f9;
+        padding: 10px;
+        color: black;
+        font-size: 24px;
+    }
+
+    #danhsach>div {
+        display: grid;
+        grid-template-columns: 90px 400px 300px 150px 50px;
+    }
+
+    #danhsach .dshead {
+        font-weight: bold;
+        font-size: 20px;
+    }
+
+    #danhsach>div>div {
+        border: 1px solid darkblue;
+        padding: 10px;
+        text-align: center;
+    }
+
+    #danhsach a {
+        text-decoration: none;
+        color: wheat;
+        font-weight: bold;
+    }
+
+    #danhsach a:hover {
+        text-decoration: underline;
+        color: black;
+    }
 </style>
